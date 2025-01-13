7050c3aedddc65fd207947198f281b3c000e0996
Bishwa <bishwakiran725@gmail.com>
2025-01-13 19:04:53 +0545

    Layout changes , navbar active highlight

--- script/event-effect.js
+++ script/event-effect.js
@@ -1,54 +1,26 @@
-const imageContainer = document.querySelector(".img-container")
-const socialContainer = document.querySelector(".social-container")
-const hoverArea = document.querySelector(".img-info-container")
-imageContainer.style.transition = ".5s all cubic-bezier(0,1.19,1,.98)"
-socialContainer.style.transition = ".5s all cubic-bezier(0,1.19,1,.98)"
-imageContainer.style.padding = "-100px"
-imageContainer.style.background = "var(--bg-color)"
-socialContainer.style.background = "var(--bg-color)"
-
-let onHover = false
-
-
-
-function hoverEffect() {
-    imageContainer.style.padding = "0px 0px 0px 25px"
-    socialContainer.style.paddingLeft = "200px"
-    socialContainer.style.paddingRight = "30px"
-    socialContainer.style.background = "rgb(64, 68, 105)"
-    imageContainer.style.background = "rgb(64, 68, 105)"
-}
-
-function unHoverEffect() {
-    imageContainer.style.padding = "0"
-    socialContainer.style.paddingLeft = "0"
-    socialContainer.style.background = "var(--bg-color)"
-    imageContainer.style.background = "var(--bg-color)"
-}
-
-window.addEventListener("load", () => {
-    
-setTimeout(() => {
-    
-    hoverArea.addEventListener('mouseover', ()=>{
-        hoverEffect()
-        onHover = true
-    })
-    hoverArea.addEventListener('mouseout', ()=>{
-        onHover = false
-        setTimeout(() => {
-            if(socialContainer.style.paddingLeft === "200px" && !onHover){
-                setTimeout(() => {
-                    unHoverEffect()
-                }, 500);
-            }
-        }, 100);
-        
-    
-    
-    })
-
-}, 1000);
-
+window.addEventListener('scroll', function() {
+    const sections = document.querySelectorAll('section');
+    const links = document.querySelectorAll('nav a');
+  
+    let currentSection = "";
+  
+    sections.forEach(section => {
+      const sectionTop = section.offsetTop;
+      const sectionHeight = section.offsetHeight;
+      const sectionBottom = sectionTop + sectionHeight;
+  
+      // Check if the section is in the viewport
+      if (window.scrollY + window.innerHeight >= sectionTop && window.scrollY < sectionBottom) {
+        currentSection = section.getAttribute('id');
+      }
+    });
+  
+    // Loop through links and add 'active' class based on scroll position
+    links.forEach(link => {
+      link.classList.remove('active');
+      if (link.getAttribute('href').includes(currentSection)) {
+        link.classList.add('active');
+      }
+    });
   });
-
+  
